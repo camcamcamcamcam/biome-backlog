1a61f9e0c14c6a58842a2bd2e4d52c21bb5349a2
baguchan <shurf012@gmail.com>
2022-08-27 21:04:22 +0900

    add starter code

---
+++ src/main/java/com/minecraftabnormals/biome_vote_losers/BiomeVoteLosers.java
@@ -0,0 +1,46 @@
+package com.minecraftabnormals.biome_vote_losers;
+
+import com.mojang.logging.LogUtils;
+import net.minecraft.client.Minecraft;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.material.Material;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.event.server.ServerStartingEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+import org.slf4j.Logger;
+
+@Mod(BiomeVoteLosers.MODID)
+public class BiomeVoteLosers
+{
+    // Define mod id in a common place for everything to reference
+    public static final String MODID = "biome_vote_losers";
+
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+
+    public BiomeVoteLosers()
+    {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        modEventBus.addListener(this::commonSetup);
+
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event)
+    {
+    }
+}
