ba0be762702674c57c33e0194324cf301c624bdf
baguchan <shurf012@gmail.com>
2022-10-31 15:18:53 +0900

    add mob spawn

---
+++ src/main/java/com/minecraftabnormals/biome_vote_losers/world/BiomeVoteModifier.java
@@ -0,0 +1,38 @@
+package com.minecraftabnormals.biome_vote_losers.world;
+
+import com.minecraftabnormals.biome_vote_losers.register.ModBiomeModifiers;
+import com.minecraftabnormals.biome_vote_losers.register.ModEntities;
+import com.mojang.serialization.Codec;
+import net.minecraft.core.Holder;
+import net.minecraft.world.entity.MobCategory;
+import net.minecraft.world.level.biome.Biome;
+import net.minecraft.world.level.biome.Biomes;
+import net.minecraft.world.level.biome.MobSpawnSettings;
+import net.minecraftforge.common.world.BiomeModifier;
+import net.minecraftforge.common.world.ModifiableBiomeInfo;
+
+public class BiomeVoteModifier implements BiomeModifier {
+	public static final BiomeVoteModifier INSTANCE = new BiomeVoteModifier();
+
+
+	@Override
+	public void modify(Holder<Biome> biome, Phase phase, ModifiableBiomeInfo.BiomeInfo.Builder builder) {
+		if (phase == Phase.ADD) {
+			if (biome.is(Biomes.SAVANNA) || biome.is(Biomes.SAVANNA_PLATEAU) || biome.is(Biomes.WINDSWEPT_SAVANNA)) {
+				builder.getMobSpawnSettings().getSpawner(MobCategory.CREATURE).add(new MobSpawnSettings.SpawnerData(ModEntities.OSTRICH.get(), 8, 2, 3));
+			}
+			if (biome.is(Biomes.DESERT)) {
+				builder.getMobSpawnSettings().getSpawner(MobCategory.CREATURE).add(new MobSpawnSettings.SpawnerData(ModEntities.MEERKAT.get(), 8, 4, 6));
+			}
+
+			if (biome.is(Biomes.BADLANDS)) {
+				builder.getMobSpawnSettings().getSpawner(MobCategory.CREATURE).add(new MobSpawnSettings.SpawnerData(ModEntities.VULTURE.get(), 6, 1, 1));
+			}
+		}
+	}
+
+	@Override
+	public Codec<? extends BiomeModifier> codec() {
+		return ModBiomeModifiers.BIOME_VOTE_SPAWN.get();
+	}
+}
